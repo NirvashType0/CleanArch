0a7e1d6d31b3a1c7aa7a54da568256f487648f82
Jose Alexander Hernandez Quezada <oiejhernan@crecer.com.sv>
2019-11-20 10:03:04 -0600

    Commit Implementation of Service  and Repository GetCourses Methods

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private S10290D0Context _ctx;
+        public CourseRepository(S10290D0Context ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
